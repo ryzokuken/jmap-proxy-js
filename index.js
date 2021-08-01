ada582e0b7d256ee0d493de417691f79d5afd75b
Ujjwal Sharma <ryzokuken@disroot.org>
2021-08-01 18:54:05 +0530

    add basic index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const fs = require('fs');
+
+const envPaths = require('env-paths');
+const Imap = require('imap');
+
+const configPath = path.join(envPaths('jmap-proxy', {suffix: ''}).config, 'config.json');
+const config = JSON.parse(fs.readFileSync(configPath).toString());
+
+const imap = new Imap(config.imap);
+
+imap.once('ready', () => {
+  imap.getBoxes((err, boxes) => {
+    if (err) console.error(err);
+    console.log(boxes);
+  });
+  imap.end();
+});
+
+process.on('SIGINT', () => {
+  console.log('shutting down');
+  imap.end();
+});
+
+imap.connect();
