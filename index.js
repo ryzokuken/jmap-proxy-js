16b4ecbe884efe92430eac3e41717048e82e5dc5
Ujjwal Sharma <ryzokuken@disroot.org>
2021-08-01 19:21:26 +0530

    comment out IMAP example, add fastify example

--- index.js
+++ index.js
@@ -3,23 +3,36 @@ const fs = require('fs');
 
 const envPaths = require('env-paths');
 // const IMAP = require('imap');
+const fastify = require('fastify')({
+  logger: true
+});
 
 const configPath = path.join(envPaths('jmap-proxy', {suffix: ''}).config, 'config.json');
 const config = JSON.parse(fs.readFileSync(configPath).toString());
 
-const imap = new IMAP(config.imap);
+// const imap = new IMAP(config.imap);
 
-imap.once('ready', () => {
-  imap.getBoxes((err, boxes) => {
-    if (err) console.error(err);
-    console.log(boxes);
-  });
-  imap.end();
+// imap.once('ready', () => {
+//   imap.getBoxes((err, boxes) => {
+//     if (err) console.error(err);
+//     console.log(boxes);
+//   });
+//   imap.end();
+// });
+
+// process.on('SIGINT', () => {
+//   console.log('shutting down');
+//   imap.end();
+// });
+
+// imap.connect();
+
+fastify.get('/', async (request, reply) => {
+  reply.type('application/json').code(200);
+  return { hello: 'world' };
 });
 
-process.on('SIGINT', () => {
-  console.log('shutting down');
-  imap.end();
+fastify.listen(3000, (err, address) => {
+  if (err) throw err;
+  console.log(`listening on ${address}`);
 });
-
-imap.connect();
